6e26f0e1b865393fd89882251393045f1d2f5034
Alex Kushev <alex.kushev.bg@gmail.com>
2017-10-19 14:18:15 +0300

    Initial commit

---
+++ src/main/java/bg/carepanda/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package bg.carepanda.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showHomePage() {
+		return "index";
+	}
+
+}
---
+++ src/main/webapp/view/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Care Panda!</h2>
+</body>
+</html>
