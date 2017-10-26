3d572991c72bcc94620e30bd8440aa41d872d26f
alexkushev <alex.kushev.bg@gmail.com>
2017-10-26 23:02:26 +0300

    commit for test purpose

--- src/main/java/bg/carepanda/controllers/HomeController.java
+++ src/main/java/bg/carepanda/controllers/HomeController.java
@@ -1,5 +1,10 @@
 package bg.carepanda.controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String showHomePage() {
+	public String showHomePage() throws SQLException, URISyntaxException {
+		
+	    URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+	    
+	    System.out.println("Test Show Message");
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    DriverManager.getConnection(dbUrl, username, password);
+		
 		return "index";
 	}
 
